7a90c165e13f322697d119cfb5e2c1b09d9f0726
sanaan <sanaan@mayahealthcare.com>
2025-11-22 01:46:45 -0600

    add: GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: {
-//       plugins: [
-//         require('@tailwindcss/postcss')()
-//       ]
-//     }
-//   },
-//   server: {
-//     port: 3000,
-//     open: true
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/unitec-website/',
   server: {
     port: 3000,
     open: true
