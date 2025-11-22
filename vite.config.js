d266c08757cf777444357bf50a861d789949bd8e
sanaan <sanaan@mayahealthcare.com>
2025-11-22 02:20:57 -0600

    add: background video to hero section

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/unitec-website/',
+  base: process.env.NODE_ENV === 'production' ? '/unitec-website/' : '/',
   server: {
     port: 3000,
     open: true
